8f40ed0b880c981f98f28b2b36867aa103f349ec
Mark Biddlecom <mark.biddlecom@redfin.com>
2020-05-16 10:47:25 -0700

    Introduction of Material UI and the beginnings of a placeholder landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import React from 'react';
-import logo from './logo.svg';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
+import 'typeface-roboto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Grid container justify="center">
+        <Grid item xs={8}>
+          <Typography variant="h1">Scratchpad <span role="img" aria-label="phone">📞</span> Telephone</Typography>
+          <Typography variant="body1">
+            This is a fun game to play with a group of friends (the more the merrier)! I'll come up with some better
+            copy much much later <span role="img" aria-label="grimacing">😬</span>
+          </Typography>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
