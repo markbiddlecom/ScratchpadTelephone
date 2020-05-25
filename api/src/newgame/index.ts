c6ba14db3ede34284a2381454988759656f619f7
Mark Biddlecom <markbiddlecom@gmail.com>
2020-05-25 09:36:46 -0700

    Adding CORS headers to lambda response

--- api/src/newgame/index.ts
+++ api/src/newgame/index.ts
@@ -12,13 +12,22 @@ export default async function newGameHandler(event: APIGatewayProxyEvent): Promi
     game = await createOrComandeerGame("TODO");
   } while (game === null && attempt < CONFIG.MAX_NEW_GAME_TRIES);
 
+  const commonResponse = {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+
   if (game) {
     return {
+      ...commonResponse,
       statusCode: 200,
       body: JSON.stringify(Object.assign({}, game, { token: CONFIG.TOKEN_FORMATTER(game.token) })),
     };
   } else {
     return {
+      ...commonResponse,
       statusCode: 420,
       body: JSON.stringify({ error: "Could not create a new game. Please try again later." }),
     };
